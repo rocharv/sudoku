21adc5e2595705143aa0e4ec4da88e9d1e1383b9
Rodrigo Viana Rocha <rodrigo.rocha@pravaler.com.br>
2020-11-30 16:10:32 -0300

    chore: all tests implemented

--- lib/sudoku.ex
+++ lib/sudoku.ex
@@ -1,35 +1,79 @@
 defmodule Sudoku do
-  def board1() do
-    %Sudoku.Board{
-      data: {
-        0, 0, 3, 6, 0, 0, 4, 0, 0,
-        6, 0, 0, 0, 8, 0, 0, 2, 0,
-        0, 9, 0, 0, 0, 5, 0, 0, 7,
-        0, 0, 4, 0, 3, 0, 0, 0, 0,
-        0, 0, 0, 2, 0, 0, 6, 0, 0,
-        2, 0, 0, 0, 0, 7, 0, 0, 5,
-        7, 0, 0, 0, 0, 0, 0, 1, 0,
-        0, 0, 6, 8, 0, 0, 0, 0, 0,
-        0, 1, 0, 0, 0, 9, 2, 0, 0},
-      grid_size: 3,
-      size: {9, 9}
-    }
+  defstruct board: {}, grid_size: 0, size: 0
+
+  def new (%Sudoku{size: size} = sudoku) do
+    board = Tuple.duplicate(0, size * size)
+    grid_size = trunc(:math.sqrt(size))
+    %Sudoku{sudoku | board: board, grid_size: grid_size}
   end
 
-  def board2() do
-    %Sudoku.Board{
-      data: {
-        0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 3, 0, 8, 5,
-        0, 0, 1, 0, 2, 0, 0, 0, 0,
-        0, 0, 0, 5, 0, 7, 0, 0, 0,
-        0, 0, 4, 0, 0, 0, 1, 0, 0,
-        0, 9, 0, 0, 0, 0, 0, 0, 0,
-        5, 0, 0, 0, 0, 0, 0, 7, 3,
-        0, 0, 2, 0, 1, 0, 0, 0, 0,
-        0, 0, 0, 0, 4, 0, 0, 0, 9},
-      grid_size: 3,
-      size: {9, 9}
-    }
+  def get(%Sudoku{board: board, size: size}, row, col) do
+    cond do
+      row < 0 or col < 0 or row >= size or col >= size ->
+        raise ArgumentError, message: "invalid row and/or column argument"
+
+      true ->
+        elem(board, row * size + col)
+    end
+  end
+
+  def put(%Sudoku{board: board, size: size} = sudoku, row, col, value) do
+    cond do
+      row < 0 or col < 0 or row >= size or col >= size or value < 1 or value > size ->
+        raise ArgumentError, message: "invalid row, column and/or value argument"
+
+      true ->
+        new_board = put_elem(board, row * size + col, value)
+        %Sudoku{sudoku | board: new_board}
+    end
+  end
+
+  def valid_put?(%Sudoku{grid_size: grid_size, size: size} = sudoku, row, col, value) do
+    cond do
+      # invalid value
+      value < 1 or value > size ->
+        raise ArgumentError, message: "invalid row and/or column argument"
+
+      # non-empty cell
+      get(sudoku, row, col) != 0 ->
+        false
+
+      # row validation
+      Enum.any?(1..size - 1, fn c -> get(sudoku, row, c) == value end) ->
+        false
+
+      # column validation
+      Enum.any?(1..size - 1, fn r -> get(sudoku, r, col) == value end) ->
+        false
+
+      # grid validation
+      true ->
+        grid_fst_row = div(row, grid_size) * grid_size
+        grid_lst_row = grid_fst_row + grid_size - 1
+        grid_fst_col = div(col, grid_size) * grid_size
+        grid_lst_col = grid_fst_col + grid_size - 1
+
+        for r <- grid_fst_row..grid_lst_row,
+            c <- grid_fst_col..grid_lst_col do
+              {r, c}
+            end
+        |> Enum.any?(fn {r, c} -> get(sudoku, r, c) == value end)
+        |> Kernel.not
+    end
+  end
+
+  def full?(%Sudoku{size: size} = board) do
+    for r <- 0..size - 1,
+        c <- 0..size - 1 do
+      {r, c}
+    end
+    |> Enum.all?(fn {r, c} -> get(board, r, c) != 0 end)
+  end
+
+  def print(%Sudoku{board: board, size: size}) do
+    board
+    |> Tuple.to_list()
+    |> Enum.chunk_every(size)
+    |> Enum.map(fn l -> Enum.join(l, ", ") |> IO.puts() end)
   end
 end
--- test/sudoku_test.exs
+++ test/sudoku_test.exs
@@ -1,7 +1,198 @@
 defmodule SudokuTest do
   use ExUnit.Case
+
+  import ExUnit.CaptureIO
+
   doctest Sudoku
-  # test "greets the world" do
-  #   assert Sudoku.hello() == :world
-  # end
+
+  setup do
+    # Testing boards
+    {:ok,
+      board_new:
+        %Sudoku{
+          board: {
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0},
+            grid_size: 3,
+            size: 9
+        },
+      board0:
+        %Sudoku{
+          board: {
+            7, 4, 3, 9, 5, 1, 6, 8, 2,
+            1, 6, 2, 4, 8, 7, 3, 9, 5,
+            9, 5, 8, 6, 3, 2, 7, 1, 4,
+            2, 1, 9, 8, 7, 3, 5, 4, 6,
+            3, 7, 4, 5, 6, 9, 1, 2, 8,
+            5, 8, 6, 1, 2, 4, 9, 7, 3,
+            4, 9, 5, 2, 1, 6, 8, 3, 7,
+            8, 2, 7, 3, 9, 5, 4, 6, 1,
+            6, 3, 1, 7, 4, 8, 2, 5, 9},
+            grid_size: 3,
+            size: 9
+        },
+      board1:
+        %Sudoku{
+          board: {
+            0, 0, 3, 6, 0, 0, 4, 0, 0,
+            6, 0, 0, 0, 8, 0, 0, 2, 0,
+            0, 9, 0, 0, 0, 5, 0, 0, 7,
+            0, 0, 4, 0, 3, 0, 0, 0, 0,
+            0, 0, 0, 2, 0, 0, 6, 0, 0,
+            2, 0, 0, 0, 0, 7, 0, 0, 5,
+            7, 0, 0, 0, 0, 0, 0, 1, 0,
+            0, 0, 6, 8, 0, 0, 0, 0, 0,
+            0, 1, 0, 0, 0, 9, 2, 0, 0},
+          grid_size: 3,
+          size: 9
+        },
+      board1_put:
+        %Sudoku{
+          board: {
+            0, 0, 3, 6, 0, 0, 4, 0, 0,
+            6, 0, 0, 0, 8, 0, 0, 2, 0,
+            0, 9, 0, 0, 0, 5, 0, 0, 7,
+            0, 0, 4, 0, 3, 0, 0, 0, 0,
+            0, 0, 0, 2, 4, 0, 6, 0, 0,
+            2, 0, 0, 0, 0, 7, 0, 0, 5,
+            7, 0, 0, 0, 0, 0, 0, 1, 0,
+            0, 0, 6, 8, 0, 0, 0, 0, 0,
+            0, 1, 0, 0, 0, 9, 2, 0, 0},
+          grid_size: 3,
+          size: 9
+        },
+      board2:
+        %Sudoku{
+          board: {
+            0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 3, 0, 8, 5,
+            0, 0, 1, 0, 2, 0, 0, 0, 0,
+            0, 0, 0, 5, 0, 7, 0, 0, 0,
+            0, 0, 4, 0, 0, 0, 1, 0, 0,
+            0, 9, 0, 0, 0, 0, 0, 0, 0,
+            5, 0, 0, 0, 0, 0, 0, 7, 3,
+            0, 0, 2, 0, 1, 0, 0, 0, 0,
+            0, 0, 0, 0, 4, 0, 0, 0, 9},
+            grid_size: 3,
+            size: 9
+        }
+    }
+  end
+
+  test "Sudoku new function test", context do
+    assert Sudoku.new(%Sudoku{size: 9}) == context[:board_new]
+  end
+
+  test "Sudoku get function test", context do
+    assert Sudoku.get(context[:board0], 0, 0) == 7
+    assert Sudoku.get(context[:board0], 0, 1) == 4
+    assert Sudoku.get(context[:board0], 0, 7) == 8
+    assert Sudoku.get(context[:board0], 0, 8) == 2
+    assert Sudoku.get(context[:board0], 4, 4) == 6
+    assert Sudoku.get(context[:board0], 8, 0) == 6
+    assert Sudoku.get(context[:board0], 8, 1) == 3
+    assert Sudoku.get(context[:board0], 8, 7) == 5
+    assert Sudoku.get(context[:board0], 8, 8) == 9
+
+    assert_raise ArgumentError, fn ->
+      Sudoku.get(context[:board0], 0, 9)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.get(context[:board0], 9, 0)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.get(context[:board0], 0, -1)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.get(context[:board0], -1, 0)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.get(context[:board0], -1, -1)
+    end
+  end
+
+  test "Sudoku put function test", context do
+    assert Sudoku.put(context[:board1], 4, 4, 4) == context[:board1_put]
+
+    assert_raise ArgumentError, fn ->
+      Sudoku.put(context[:board0], 0, 9, 1)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.put(context[:board0], 9, 0, 1)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.put(context[:board0], 9, 9, 1)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.put(context[:board0], 0, 0, 10)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.put(context[:board0], 0, 0, 0)
+    end
+  end
+
+  test "Sudoku valid_put? function test", context do
+    assert Sudoku.valid_put?(context[:board1], 0, 0, 1) == true
+    assert Sudoku.valid_put?(context[:board1], 0, 0, 2) == false
+    assert Sudoku.valid_put?(context[:board1], 0, 0, 3) == false
+    assert Sudoku.valid_put?(context[:board1], 0, 0, 4) == false
+    assert Sudoku.valid_put?(context[:board1], 0, 0, 5) == true
+    assert Sudoku.valid_put?(context[:board1], 0, 0, 6) == false
+    assert Sudoku.valid_put?(context[:board1], 0, 0, 7) == false
+    assert Sudoku.valid_put?(context[:board1], 0, 0, 8) == true
+    assert Sudoku.valid_put?(context[:board1], 0, 0, 9) == false
+
+    assert Sudoku.valid_put?(context[:board1], 4, 4, 1) == true
+    assert Sudoku.valid_put?(context[:board1], 4, 4, 2) == false
+    assert Sudoku.valid_put?(context[:board1], 4, 4, 3) == false
+    assert Sudoku.valid_put?(context[:board1], 4, 4, 4) == true
+    assert Sudoku.valid_put?(context[:board1], 4, 4, 5) == true
+    assert Sudoku.valid_put?(context[:board1], 4, 4, 6) == false
+    assert Sudoku.valid_put?(context[:board1], 4, 4, 7) == false
+    assert Sudoku.valid_put?(context[:board1], 4, 4, 8) == false
+    assert Sudoku.valid_put?(context[:board1], 4, 4, 9) == true
+
+    assert_raise ArgumentError, fn ->
+      Sudoku.valid_put?(context[:board1], 0, 9, 1)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.valid_put?(context[:board1], 9, 0, 1)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.valid_put?(context[:board1], 9, 9, 1)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.valid_put?(context[:board1], 0, 0, 10)
+    end
+    assert_raise ArgumentError, fn ->
+      Sudoku.valid_put?(context[:board1], 0, 0, 0)
+    end
+  end
+
+  test "Sudoku print function test", context do
+    assert capture_io(fn -> Sudoku.print(context[:board0]) end) ==
+      ~s"""
+      7, 4, 3, 9, 5, 1, 6, 8, 2
+      1, 6, 2, 4, 8, 7, 3, 9, 5
+      9, 5, 8, 6, 3, 2, 7, 1, 4
+      2, 1, 9, 8, 7, 3, 5, 4, 6
+      3, 7, 4, 5, 6, 9, 1, 2, 8
+      5, 8, 6, 1, 2, 4, 9, 7, 3
+      4, 9, 5, 2, 1, 6, 8, 3, 7
+      8, 2, 7, 3, 9, 5, 4, 6, 1
+      6, 3, 1, 7, 4, 8, 2, 5, 9
+      """
+  end
+
+  test "Sudoku full? function test", context do
+    assert Sudoku.full?(context[:board0]) == true
+    assert Sudoku.full?(context[:board1]) == false
+  end
 end
